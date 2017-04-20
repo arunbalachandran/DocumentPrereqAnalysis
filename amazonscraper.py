0e86e2adff44d19e5beb54191461ac186c30a330
Arun Balachandran <balachandran.arun94@gmail.com>
2017-04-20 01:50:18 -0500

    removed non ascii character

--- amazonscraper.py
+++ amazonscraper.py
@@ -8,10 +8,10 @@ import os
 # }
 
 config = {
-    'access_key': os.environ.get('AMAZON_ACCESS'),
-    'secret_key': os.environ.get('AMAZON_SECRET'),
-    'associate_tag': os.environ.get('AMAZON_ASSOCIATE'),
-    'locale': os.environ.get('AMAZON_LOCALE')
+'access_key': os.environ.get('AMAZON_ACCESS'),
+'secret_key': os.environ.get('AMAZON_SECRET'),
+'associate_tag': os.environ.get('AMAZON_ASSOCIATE'),
+'locale': os.environ.get('AMAZON_LOCALE')
 }
 
 def get_products(keyword):
@@ -20,15 +20,15 @@ def get_products(keyword):
     html_list = []
     for book in products:  # probably limits itself to 50 books
         html = '''<div class='product-box'>
-                    <a target="_blank" href="{}">
-                      <img src="{}" width="120" height="160">
-                    </a>
-                    <div class="product-title">
-                      <h3>{}</h3>
-                      <p class="product-price">{} {}<br></p>
-                    </div>
-                  </div>'''.format(book.detail_page_url,
-                                   book.large_image_url,
-                                   book.title, book.price_and_currency[1], book.price_and_currency[0])
+        <a target="_blank" href="{}">
+        <img src="{}" width="120" height="160">
+        </a>
+        <div class="product-title">
+        <h3>{}</h3>
+        <p class="product-price">{} {}<br></p>
+        </div>
+        </div>'''.format(book.detail_page_url,
+        book.large_image_url,
+        book.title, book.price_and_currency[1], book.price_and_currency[0])
         html_list.append(html)
     return ''.join(html_list[:10])
